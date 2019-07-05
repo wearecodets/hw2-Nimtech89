220573c6e9a133f8305d246a075b10e115ce2d8c
Nima Zarei <nimazarei89@gmail.com>
2019-07-05 19:32:42 +0700

    trying to finish part 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello, world!');
+const form = document.querySelector('.form');
+form.plan.forEach(function(plan) {
+  plan.addEventListener('change', function() {
+    console.log(form.plan.value);
+  });
+});
+
+document.querySelector('form__total').textContent = '$0';
