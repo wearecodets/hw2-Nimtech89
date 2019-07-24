e3e91cb3fd034e05b82483e4201f3b2eb3b0d8c3
Nima Zarei <nimazarei89@gmail.com>
2019-07-24 19:24:15 +0700

    fix all

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-console.log('Hello, world!');
+const monthlyCost = 30;
+const annualCost = 25;
+let amount = 0;
+
+const form = document.querySelector('.form');
+const total = document.querySelector('.form__total');
+const paypal = document.querySelector('a.form__cc-heading');
+
+function calculateTotal() {
+  const frequency = form.plan.value;
+  amount = frequency === 'Monthly' ? monthlyCost : annualCost * 12;
+}
+
+function updateTotal() {
+  form.submit.disabled = false;
+  total.textContent = `$${amount}`;
+  form.submit.textContent = `Pay $${amount} Now`;
+  paypal.setAttribute('href', `https://paypal.me/nikosolihin/${amount}`);
+}
+
+form.plan.forEach(function(plan) {
+  plan.addEventListener('change', function() {
+    calculateTotal();
+    updateTotal();
+  });
+});
